3e560e3f9f8afefc41dd56231bd420f7ff4cc19c
Joseph Cornelius <josephcornelius02@outlook.com>
2023-03-29 16:22:37 -0700

    code

--- script.js
+++ script.js
@@ -1,134 +1,30 @@
 var generateBtn = document.querySelector("#generate");
 
-var upperCase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
-];
-
-var lowerCase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
-    ];
-
-var numbers = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9"
-    ];
-var symbols = [
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "+",
-    "-",
-    "=",
-    "{",
-    "}",
-    "[",
-    "]",
-    "|",
-    ":",
-    ";",
-    "'",
-    "<",
-    ">",
-    "?",
-    "/",
-    "`",
-    "~"
-    ];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
 var pwLength = "";
 var characrterSet = "";
-var confirmUpper = "";
-var confirmLower = "";
-var confirmNumber = "";
-var confirmSymbol = "";
-
 
 
 // Write password to the #password input
 function writePassword() {
-var length = prompt("How many characters would you like your password to contain?");
-if (length < 8) {
-    alert("Password must be at least 8 characters");
-    writePassword();
-} else if (length > 128) {
-    alert("Password must be less than 128 characters");
-    writePassword();
-} else {
+    var length = prompt("Choose a length between 8-128 characters");
+    if (length < 8 || length > 128) {
+      writePassword();
+      alert("Password length is either too short or too long, try again");
+      prompt("Choose a length between 8-128 characters");
+    }
     console.log(length);
 };
 
-var newPw = "";
 var confirmUpper = confirm("Do you want to use uppercase letters?");
 var confirmLower = confirm("Do you want to use lowercase letters?");
 var confirmNumber = confirm("Do you want to use numbers?");
 var confirmSymbol = confirm("Do you want to use symbols?");
-
+var password = "";
 
 if(confirmUpper) {
     characrterSet += upperCase;
@@ -165,13 +61,8 @@ if(confirmUpper === false
     console.log(newPw);
 
     var passwordText = document.querySelector("#password");
-
     passwordText.value = newPw;
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-};
-
-return newPw;
